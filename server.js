ab36760daad65074e21bfe6f11e034f5c5f5e800
kindluddite <howley.phil@gmail.com>
2018-09-08 11:38:01 -0700

    connected mongo

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose'); // add this
+require('dotenv').config({ path: 'variables.env' });
 
 const PORT = process.env.PORT || 4444;
+
+// connect to db (add these lines)
+mongoose
+  .connect(
+    process.env.MONGO_URI,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log('DB connected');
+  })
+  .catch(err => {
+    console.log('Error on start: ' + err.stack);
+    process.exit(1);
+  });
+
+const app = express();
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
