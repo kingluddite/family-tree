541b832b96aae7e60f094ddd2d9849b4cc45b111
kindluddite <howley.phil@gmail.com>
2018-09-08 12:39:26 -0700

    Add graphql schema

--- schema.js
+++ schema.js
@@ -1,1 +1,18 @@
-exports.typeDefs = ``;
+exports.typeDefs = `
+  type Genealogy {
+    firstName: String!
+    lastName: String!
+    dateOfBirth: String
+    createdDate: String
+    description: String
+    likes: Int
+    username: String
+  }
+  type User {
+    username: String!
+    password: String!
+    email: String!
+    joinDate: String
+    favorites: [Genealogy]
+  }
+`;
